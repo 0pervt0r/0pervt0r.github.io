d7098be3e3b280bd6d25375c5513c80f47d433c0
0pervt0r <dlafikbuka762@gmail.com>
2026-05-28 21:49:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function toggleAccordion(trigger) {
   var item = trigger.closest('.accordion-item');
+  var body = item.querySelector('.accordion-body');
   var isOpen = item.classList.contains('open');
-  item.classList.toggle('open', !isOpen);
+
+  if (isOpen) {
+    body.style.maxHeight = body.scrollHeight + 'px';
+    requestAnimationFrame(function() {
+      body.style.maxHeight = '0';
+    });
+    item.classList.remove('open');
+  } else {
+    item.classList.add('open');
+    body.style.maxHeight = body.scrollHeight + 'px';
+    body.addEventListener('transitionend', function handler() {
+      body.style.maxHeight = 'none';
+      body.removeEventListener('transitionend', handler);
+    });
+  }
 }
